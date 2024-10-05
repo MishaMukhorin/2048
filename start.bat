ed68b5c56a156c7cecca159934d20cff9e85d7b4
Misha Mukhorin <misha.mukhorin@icloud.com>
2024-10-05 15:13:55 +0300

    fix

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ echo Building Docker image...
 docker build -t 2048-game-image .
 
 echo 1
-docker run -d --name 2048-game-1 -p 8080:8080 2048-game-image
+docker run -d --name 2048-1 -p 8080:8080 2048-game-image
 
 echo 2
-docker run -d --name 2048-game-2 -p 8081:8080 2048-game-image
+docker run -d --name 2048-2 -p 8081:8080 2048-game-image
 
 echo done
